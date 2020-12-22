a8e9ce6470d4f747c15f8c8ffbb18bd29e01dd9f
siddacious <nospam187+github@gmail.com>
2020-12-21 16:14:02 -0800

    added ALS; mode switch is buggy

--- adafruit_ltr390.py
+++ adafruit_ltr390.py
@@ -43,7 +43,7 @@ _MEAS_RATE = const(0x04)  # Resolution and data rate
 _GAIN = const(0x05)  # ALS and UVS gain range
 _PART_ID = const(0x06)  # Part id/revision register
 _STATUS = const(0x07)  # Main status register
-# _ALSDATA = const(0x0D)         # ALS data lowest byte
+_ALSDATA = const(0x0D)         # ALS data lowest byte
 _UVSDATA = const(0x10)  # UVS data lowest byte
 # _INT_CFG = const(0x19)         # Interrupt configuration
 # _INT_PST = const(0x1A)         # Interrupt persistance config
@@ -101,6 +101,7 @@ class LTR390:
     # def __init__(self, register_address, struct_format, bitwidth, length):
 
     _uvs_data = UnalignedStruct(_UVSDATA, "<I", 24, 3)
+    _als_data = UnalignedStruct(_ALSDATA, "<I", 24, 3)
     """Ask the sensor if new data is available"""
 
     def __init__(self, i2c, address=_DEFAULT_I2C_ADDR):
@@ -111,8 +112,10 @@ class LTR390:
 
         self.reset()
         self._mode = 1  # UV index/UVS, ALS=0
+        self._mode_cache = self._mode
         self._gain = 1  # GAIN_3
         self._resolution = 4  # RESOLUTION_16BIT
+
         # ltr.setThresholds(100, 1000);
         # ltr.configInterrupt(true, LTR390_MODE_UVS);
 
@@ -137,9 +140,25 @@ class LTR390:
     def initialize(self):
         """Configure the sensor with sensible defaults so it can be used"""
 
+    # something is wrong here; I had to add a sleep to the loop to get both to update correctly
     @property
     def uv_index(self):
         """The calculated UV Index"""
+        if self._mode_cache != 1:
+            self._mode = 1
+            self._mode_cache = 1
+        while not self.data_ready:
+            print("DRsleep")
+            sleep(0.010)
         return self._uvs_data
-        # self._take_reading()
-        # return ((self._uvacalc * self._uvaresp) + (self._uvbcalc * self._uvbresp)) / 2
+
+    @property
+    def light(self):
+        """The currently measured ambient light level"""
+        if self._mode_cache != 0:
+            self._mode = 0
+            self._mode_cache = 0
+        while not self.data_ready:
+            print("DRsleep")
+            sleep(0.010)
+        return self._als_data
--- examples/ltr390_simpletest.py
+++ examples/ltr390_simpletest.py
@@ -11,4 +11,6 @@ i2c = busio.I2C(board.SCL, board.SDA)
 ltr = adafruit_ltr390.LTR390(i2c)
 while True:
     print("UV Index:", ltr.uv_index)
+    time.sleep(1)
+    print("Ambient Light:", ltr.light)
     time.sleep(1.0)
